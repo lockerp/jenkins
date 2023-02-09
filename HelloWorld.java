c29ea1dc3374ab66d0ab2310edf8a282b053f469
lockerp <47007076+lockerp@users.noreply.github.com>
2023-02-09 12:17:18 -0600

    Update and rename HelloWorld.java to test.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-        System.out.println("Hello, World, 2nd");
-         System.out.println("Hello, World, 3nd");
-        System.out.println("Hello, World, 4");
-    }
-}
